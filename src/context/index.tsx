ac826490f06aba213ec6fb407751ca74ce0ab4df
nimod7890 <nimod1234@g.skku.edu>
2024-07-15 21:52:15 +0900

    rename: todo context files

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
-import TodoProvider from "./todo";
+import TodoProvider from "./todo/provider";
 
 export default function ContextProvider({ children }: PropsWithChildren) {
   return <TodoProvider>{children}</TodoProvider>;
