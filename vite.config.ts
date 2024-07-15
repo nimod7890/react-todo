6161b89d0e4644f88c0ca25045b4308b011f6657
nimod7890 <nimod1234@g.skku.edu>
2024-07-15 22:42:59 +0900

    chore: cache 저장 위치 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  cacheDir: "./.vite",
+  cacheDir: "./.yarn/.vite",
   server: { port: 3000 },
 });
