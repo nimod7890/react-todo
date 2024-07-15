81a064802d39b3a8adba8686ed6079b4895dda9d
nimod7890 <nimod1234@g.skku.edu>
2024-07-15 22:01:34 +0900

    style: add blank lines between code for readability

--- src/context/search/index.ts
+++ src/context/search/index.ts
@@ -3,6 +3,10 @@ import { SearchContext } from "./context";
 
 export default function useSearchContext() {
   const context = useContext(SearchContext);
-  if (!context) throw new Error("useSearchContext must be used within a SearchProvider");
+
+  if (!context) {
+    throw new Error("useSearchContext must be used within a SearchProvider");
+  }
+
   return context;
 }
--- src/context/todo/index.ts
+++ src/context/todo/index.ts
@@ -3,8 +3,10 @@ import { TodoContext } from "./context";
 
 export default function useTodoContext() {
   const context = useContext(TodoContext);
+
   if (!context) {
     throw new Error("useTodoContext must be used within a TodoProvider");
   }
+
   return context;
 }
