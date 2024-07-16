5cdd7659b1f5fdc56c14ed5b9057ca045777cc77
nimod7890 <nimod1234@g.skku.edu>
2024-07-16 15:05:23 +0900

    rename: user context state

--- src/store/provider/UserContextProvider.tsx
+++ src/store/provider/UserContextProvider.tsx
@@ -4,7 +4,7 @@ import userReducer from "src/store/reducer/userReducer";
 import { LoginUserProps, USER_ACTION } from "src/store/types/userTypes";
 
 const UserContextProvider = ({ children }: PropsWithChildren) => {
-  const [state, dispatch] = useReducer(userReducer, initialUser);
+  const [user, dispatch] = useReducer(userReducer, initialUser);
 
   const login = (payload: LoginUserProps) => {
     dispatch({ type: USER_ACTION.LOGIN_USER, payload });
@@ -14,9 +14,7 @@ const UserContextProvider = ({ children }: PropsWithChildren) => {
     dispatch({ type: USER_ACTION.LOGOUT_USER });
   };
 
-  return (
-    <UserContext.Provider value={{ user: state, login, logout }}>{children}</UserContext.Provider>
-  );
+  return <UserContext.Provider value={{ user, login, logout }}>{children}</UserContext.Provider>;
 };
 
 export default UserContextProvider;
