171c57c5a93e8142da9a72c56aa0d4c95f8602ca
nimod7890 <nimod1234@g.skku.edu>
2024-07-16 10:49:18 +0900

    remove: contexts

--- src/contexts/log/context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { Log } from "src/models/log";
-
-type LogContextType = {
-  logs: Log[];
-  addLog: (entry: Log) => void;
-};
-
-export const LogContext = createContext<LogContextType | undefined>(undefined);
--- src/contexts/search/context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-type SearchContextType = {
-  recentSearches: string[];
-  addSearchTerm: (term: string) => void;
-  deleteSearchTerm: (term: string) => void;
-};
-
-export const SearchContext = createContext<SearchContextType | undefined>(undefined);
